3d29ca3d5886dad52b99e56273f8e4bb395a0823
Sristi <sristikundu2468@gmail.com>
2026-03-02 17:42:45 +0600

    add the value in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Blogs from "./components/Blogs/Blogs";
 import ReadBlogList from "./components/ReadBlogList/ReadBlogList";
+import { addBlogsToLocalstorage, getBlogFromLocalstorage } from "./components/utilites/Localstorage";
 
 // fetch blog json data
 const blogPromises = fetch("blogs.json").then((res) => res.json());
@@ -14,15 +15,24 @@ function App() {
   const handleMarkedBlog = (blog) => {
     const newReadBlog = [...readBlog, blog];
     setReadBlog(newReadBlog);
-    console.log(readBlog);
+
+    // Store blog id in localStorage
+    addBlogsToLocalstorage(blog.id);
   };
 
 
+   //  Get bookmarked IDs from localStorage
+  const storedBlogsIds = getBlogFromLocalstorage();
+
+  // Filter blogs to show only bookmarked blogs
+  const bookmarkedBlogs = readBlog.filter((blog) => storedBlogsIds.includes(blog.id));
+
+  // count total reading time
   let totalReadingTime = 0;
 
-  for (const blog of readBlog) {
-  totalReadingTime += blog.reading_time;
-}
+  for (const blog of bookmarkedBlogs) {
+    totalReadingTime += blog.reading_time;
+  }
 
   return (
     <>
@@ -42,14 +52,18 @@ function App() {
 
           {/* right side selected blogs  */}
           <div className="col-span-1 py-6">
-             <h1 className='border border-[#6047EC] rounded-lg bg-[#6047EC]/10 text-[#6047EC] text-lg mb-3 font-bold py-5 px-6'>Spent time on read : <span>{totalReadingTime}</span> min</h1>
+            <h1 className="border border-[#6047EC] rounded-lg bg-[#6047EC]/10 text-[#6047EC] text-lg mb-3 font-bold py-5 px-6">
+              Spent time on read : <span>{totalReadingTime}</span> min
+            </h1>
 
-             <div className="rounded-lg bg-[#111111]/5 p-5">
-              <h1 className='text-[#111111] text-lg font-bold text-left'>Bookmarked Blogs : <span>{readBlog.length}</span></h1>
-  {readBlog.map((blog) => <ReadBlogList key={blog.id} blog={blog} ></ReadBlogList>)}
-             </div>
-
-          
+            <div className="rounded-lg bg-[#111111]/5 p-5">
+              <h1 className="text-[#111111] text-lg font-bold text-left">
+                Bookmarked Blogs : <span>{bookmarkedBlogs.length}</span>
+              </h1>
+              {bookmarkedBlogs.map((blog) => (
+                <ReadBlogList key={blog.id} blog={blog}></ReadBlogList>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/components/utilites/Localstorage.jsx
+++ src/components/utilites/Localstorage.jsx
@@ -3,9 +3,25 @@ const getBlogFromLocalstorage = () =>{
     const bookMarkedBlogs = localStorage.getItem('Bookmarked');
 
     if(bookMarkedBlogs){
-        const storeBlog = JSON.parse(storeBlog);
+        const storeBlog = JSON.parse(bookMarkedBlogs);
         return storeBlog
     }
 
     return [];
 }
+
+
+// save data in localstorage 
+
+const saveBlogInLocalstorage = (Bookmarked) =>{
+const bookmarkedString = JSON.stringify(Bookmarked);
+localStorage.setItem('Bookmarked', bookmarkedString)
+}
+
+const addBlogsToLocalstorage = (id) =>{
+const bookmarkedBlogs = getBlogFromLocalstorage();
+const newBlogs = [...bookmarkedBlogs,id];
+saveBlogInLocalstorage(newBlogs);
+}
+
+export{getBlogFromLocalstorage,addBlogsToLocalstorage}
